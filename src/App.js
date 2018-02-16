6a3ba5ea6166b05c04c49023c389803dadd95bd2
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-16 08:08:30 +0100

    53. Lists & State

--- src/App.js
+++ src/App.js
@@ -14,19 +14,6 @@ class App extends Component {
     showPersons: false
   }
 
-  //IS6 Arrow Function
-  switchNameHandler = (newName) => {
-    // console.log("Was clicked");
-    // TO NIE DZIAŁAthis.state.persons[0].name = "Bartłomiej";
-    this.setState({
-      persons: [
-      { name: newName, age: 22},
-      { name: 'Noziak', age: 32},
-      { name: 'Kupa', age: 5}
-      ]
-    })
-  };
-
   nameChangedHandler  = (event) => {
     this.setState({
       persons: [
@@ -41,6 +28,12 @@ class App extends Component {
     const doesShow = this.state.showPersons;
     this.setState({showPersons: !doesShow});
   };
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
 
   render() {
     // INLINE STYLING
@@ -57,8 +50,8 @@ class App extends Component {
     if (this.state.showPersons){
       persons  = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person  name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} />
           })}
       </div>
       );
