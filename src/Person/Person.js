fc1266887a96715b4af99d1aa88a2d1b06dce061
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-16 04:26:38 +0100

    34. Outputting Dynamic Content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //IS6
 const person = () => {
-    return <p>Jestem osobą</p>
+    return <p>Jestem osobą i mam {Math.floor(Math.random()*30)} lat</p>
 };
 
 export default person;
