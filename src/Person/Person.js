34505046622a892ff1d1c1dd19aa24da6e74b77f
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-17 23:24:15 +0100

    75. Using Error Boundaries (React 16+)

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,13 @@ import classes from './Person.css';
 
 //IS6
 const person = (props) => {
+    const rnd = Math.random();
+
+        if (rnd > 0.7){
+            throw new Error("O kurwa!");
+        }
     return (
+        
         <div className={classes.Person}>
             <p onClick={props.click}>Jestem {props.name} i mam {props.age} lat</p>
             <p>{props.children}</p>
