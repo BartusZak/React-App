9bd89208c60b1db522e8cbef843ac6068ec9c152
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-17 23:51:32 +0100

    79. A Better Project Structure

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './Person.css';
+
+//IS6
+const person = (props) => {
+    const rnd = Math.random();
+
+        // if (rnd > 0.7){
+        //     throw new Error("O kurwa!");
+        // }
+    return (
+        
+        <div className={classes.Person}>
+            <p onClick={props.click}>Jestem {props.name} i mam {props.age} lat</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
+        </div>    
+    )
+};
+
+export default person;
---
+++ src/containers/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+//dodaje plik żeby stworzyć component
+import Person from '../components/Persons/Person/Person';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'asd123', name: 'Bartek', age: 22},
+      { id: '1wsd', name: 'Noziak', age: 32},
+      { id: 'asdwww1', name: 'Kupa', age: 10}
+    ],
+    otherState: 'balalala',
+    showPersons: false
+  }
+
+  nameChangedHandler  = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    
+    //modern approach spread operator
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //alternative 
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+    
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons} );
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  };
+
+  deletePersonHandler = (personIndex) => {
+    //slice tworzy kopJe
+    //const persons = this.state.persons.slice;
+    //alternatywa bardziej nowoczesne
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  render() {
+    let persons = null;
+    let btnClass = '';
+
+    if (this.state.showPersons){
+      persons  = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id} >
+              <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age} 
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+              </ErrorBoundary>
+          })}
+      </div>
+      );
+
+      btnClass = classes.Red;
+    }
+
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red); 
+    }
+
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
+    }
+
+     return (
+        <div className={classes.App}>
+          <h1>Siema ziomy!</h1>
+          <p className={assignedClasses.join(' ')}>To na prawede działa!</p>
+
+          {/* NOT RECCOMENDED using ARROW Function */}
+          <button  className={btnClass} onClick={this.togglePersonHandler}>Toggle Osoby</button>
+          {persons}        
+        </div>
+    ); 
+    //return React.createElement('div', {className:  'App'}, React.createElement('h1', null, 'co tam'));
+  }
+}
+
+export default App;
