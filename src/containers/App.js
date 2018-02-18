5b448df8a823a5daff9b9872b8284fbf89546319
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-18 17:36:03 +0100

    80. Splitting an App Into Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 //dodaje plik żeby stworzyć component
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
 import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -52,42 +53,21 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = '';
 
     if (this.state.showPersons){
-      persons  = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id} >
-              <Person 
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age} 
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
-              </ErrorBoundary>
-          })}
-      </div>
-      );
-
-      btnClass = classes.Red;
+      persons  = <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}/>
     }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); 
-    }
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-    }
-
      return (
         <div className={classes.App}>
-          <h1>Siema ziomy!</h1>
-          <p className={assignedClasses.join(' ')}>To na prawede działa!</p>
+          
+          <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons} 
+          clicked={this.togglePersonHandler}/>
 
-          {/* NOT RECCOMENDED using ARROW Function */}
-          <button  className={btnClass} onClick={this.togglePersonHandler}>Toggle Osoby</button>
           {persons}        
         </div>
     ); 
